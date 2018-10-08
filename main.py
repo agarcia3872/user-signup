3161084363f62afb6deb79dd20ae9f2273bed222
Alex Garcia <bolso72@gmail.com>
2018-10-08 19:51:02 -0400

    Fixed the main.py and user-signup.html so the user input is preserved after submit button is pressed.

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ def validate_fields():
         error1 = "That's not a valid username"
         error_string = error_string + "error1=" + error1 + "&"
         isError = True
+        return redirect("/?error=" + error2 +"&username=" + username)
         #return redirect("/?error=" + error)
+    else:
+        SavedInputString = "&username=" + username
     if not password or len(password) < 3 or len(password) > 20 or " " in password:
         error2 = "That's not a valid password"
         error_string = error_string + "error2=" + error2 + "&"
@@ -29,21 +32,28 @@ def validate_fields():
         error_string = error_string + "error3=" + error3 + "&"
         isError = True
         #return redirect("/?error=" + error)
-    if '@' not in email or '.' not in email or len(email) < 3 or len(email) > 20 or " " in email:
+    if not email:
+        email
+    elif '@' not in email or '.' not in email or len(email) < 3 or len(email) > 20 or " " in email:
         error4 = "That's not a valid email"
         error_string = error_string + "error4=" + error4 + "&"
         isError= True
+    else:
+        SavedInputString =  SavedInputString + "&email=" + email
     if isError == True:
-        return redirect(error_string)
+        return redirect(error_string + SavedInputString)
     else:
         return render_template('welcome.html', username=username)
 
 @app.route("/")
 def index():
+    encoded_username = request.args.get("username")
+    encoded_email = request.args.get("email")
     encoded_error1 = request.args.get("error1")
     encoded_error2 = request.args.get("error2")
     encoded_error3 = request.args.get("error3")
     encoded_error4 = request.args.get("error4")
-    return render_template('user-signup.html', error1=encoded_error1, error2=encoded_error2, error3=encoded_error3, error4=encoded_error4)
+    return render_template('user-signup.html', 
+    username=encoded_username, email=encoded_email, error1=encoded_error1, error2=encoded_error2, error3=encoded_error3, error4=encoded_error4)
 
 app.run()
